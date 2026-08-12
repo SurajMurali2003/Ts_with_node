38c6f68ba14ef723b14b3f786421890e9ee0d82d
SurajMurali2003 <surajmuralisria@gmail.com>
2026-08-12 17:19:47 +0530

    first commit

---
+++ dist/app.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Hello from node");
+const getName = (name) => {
+    return name;
+};
+console.log(getName("SurajMurali"));
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,7 @@
+console.log("Hello from node");
+
+const getName = (name: String) => {
+   return name
+};
+
+console.log(getName("SurajMurali"));
