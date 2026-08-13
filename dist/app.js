40679946b44021278a6a346e9f66c764aca1673d
ilarumjarus <ilarumjarus@gmail.com>
2026-08-13 06:17:56 +0530

    TS-started

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,33 @@
 "use strict";
+// Basic types
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Hello from node");
-const getName = (name) => {
-    return name;
-};
-console.log(getName("SurajMurali"));
+const nodeFs = require("node:fs");
+const name = "Murali";
+const age = 50;
+const rich = true;
+// any type -> ts will not check for type
+let status = "happy";
+status = 100;
+status = true;
+const lands = ["chit-Farm", "TRL-House", "Senji-Flat"];
+// Enums
+// enum -> is used to define a set of named constant values.
+var Approval;
+(function (Approval) {
+    Approval[Approval["pending"] = 0] = "pending";
+    Approval[Approval["approved"] = 1] = "approved";
+    Approval[Approval["rejected"] = 2] = "rejected";
+})(Approval || (Approval = {}));
+var Direction;
+(function (Direction) {
+    Direction[Direction["up"] = 0] = "up";
+    Direction[Direction["down"] = 1] = "down";
+    Direction[Direction["left"] = 2] = "left";
+    Direction[Direction["rights"] = 3] = "rights";
+})(Direction || (Direction = {}));
+const curWay = Direction?.up;
+const reportStatus = Approval?.pending;
+const reportStatus1 = Approval?.rejected;
+console.log("reportStatus", reportStatus1);
+console.log("curWay", curWay);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,38 @@
-console.log("Hello from node");
+// Basic types
 
-const getName = (name: String) => {
-   return name
-};
+import nodeFs = require("node:fs");
 
-console.log(getName("SurajMurali"));
+const name: string = "Murali";
+
+const age: number = 50;
+
+const rich: boolean = true;
+
+// any type -> ts will not check for type
+let status: any = "happy";
+status = 100;
+status = true;
+
+const lands: string[] = ["chit-Farm", "TRL-House", "Senji-Flat"];
+
+// Enums
+// enum -> is used to define a set of named constant values.
+enum Approval {
+  pending = 0,
+  approved = 1,
+  rejected = 2,
+}
+
+enum Direction {
+  up,
+  down,
+  left,
+  rights,
+}
+
+const curWay: Direction = Direction?.up;
+const reportStatus: Approval = Approval?.pending;
+const reportStatus1: Approval = Approval?.rejected;
+
+console.log("reportStatus", reportStatus);
+console.log("curWay", curWay);
